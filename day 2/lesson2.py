84e6197a2014b96907254544df588539360eb158
luka1sys <dgebuadzeluka2008@gmail.com>
2024-10-07 13:10:30 +0400

    first commit

---
+++ day 2/lesson2.py
@@ -0,0 +1,25 @@
+#name ="luka"
+#name არის ცვლადი
+# = არის ცვლადისსთვის მნისჰვნელობის მიმნიჭებელი სიმბოლო
+# luka არის ცვლადისტვის მნიშვნელობა 
+surname = "dgebuadze"
+
+#print(name)
+# print ფუნქციას გადაეცემა  ეკრანზე გამოსატანი ობიექტი
+name = "luka" # ეს არის str (string) ტიპის ცვლადი
+age = 15 #int(integer) მთელი რიცხვი
+heigh = 170.5 # ეს არის floot ტიპის ცვლადი 
+# boolean (bool) ტიპის ცვლადი
+knows_programming = True
+is_ugly = False
+
+print(name + " " +  surname)
+print(name  + " "+ str(age))
+input("l am "+name+ " " +("my last name") + "  "  + surname+" "+("and l am ") + str (age )  + " " + "years old")
+name = "luka"
+
+surname = "dgebuadze"
+
+age = "15"
+
+
