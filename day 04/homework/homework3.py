915aa3d87832e0af0340586df0c93950c19249cb
luka1sys <dgebuadzeluka2008@gmail.com>
2024-10-12 21:47:19 +0400

    second commit

---
+++ day 04/homework/homework3.py
@@ -0,0 +1,32 @@
+#name: "luka"    #არასწორია 
+
+name= "luka" #სწორია
+
+
+#dog name="baxi"   #არასწორია
+
+dog_name="baxi"     #სწორია
+
+#name="luka"
+#print(name+surname)         #არასწორია
+#surname="dgebuadze"        
+
+
+name="luka"
+surname="dgebuadze"      #სწორია
+print(name+surname)  
+
+
+#salary=400
+#print(new_salary)             #არასწორია
+#new_salary=salary+300
+
+
+salary=400
+new_salary=salary+300         #სწორია
+print(new_salary)
+
+
+
+
+
---
+++ day 04/homework/homework5.py
@@ -0,0 +1,6 @@
+age1=15
+age2=23
+age3=20
+age4=50
+age5=54
+print(age1+age2+age3+age4+age5+age5)
